b40dfd6cb090ce24f30f22502cf4abde8c742288
capy <capy.net@gmail.com>
2013-02-25 05:56:24 +0100

    creo un plugin style para views. aplico un poco de estilos en general.

---
+++ sites/all/modules/custom/aed_mod/views-view-tabs.tpl.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a rows in a grid.
+ *
+ * - $rows contains a nested array of rows. Each row contains an array of
+ *   columns.
+ *
+ * @ingroup views_templates
+ */
+//We nedd ensure thats no conflict if more than one views tab
+$seed = rand(0, 9990);
+?>
+
+<div class="views-tabs clearfix">
+    <ul>
+      <?php for ($i = 0; $i < count($tabs); $i++): ?>
+        <li><a href="#tab-content-<?php print $i . "-" . $seed; ?>"><?php print $tabs[$i]; ?></a></li>
+      <?php endfor; ?>
+    </ul>
+
+  <?php for ($i = 0; $i < count($content_tabs); $i++): ?>
+    <div id="tab-content-<?php print $i . "-" . $seed; ?>">
+      <?php foreach ($content_tabs[$i] as $item): ?>
+      <?php print $item; ?>
+      <?php endforeach; ?>
+    </div>
+  <?php endfor; ?>
+
+</div>
